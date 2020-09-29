a05e7884f89d355666cc8acb86143441f7e48b00
Julian Veit <claymore1298@gmail.com>
2020-09-29 15:04:31 +0200

    Âhotdog: rootdir: add init.aicp-sm8150.rc to bring in line with guacamole
    Change-Id: I2d508b5d476bfbb062ad246445785896cb4455a0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -24,3 +24,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.hotdog.rc
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.aicp-sm8150.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.aicp-sm8150.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
