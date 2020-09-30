183823f9c0f4ddced893d5d613f75e03c8276ecc
Julian Veit <claymore1298@gmail.com>
2020-09-30 09:19:49 +0200

    hotdog: audio: add dax-default.xml
    Change-Id: Id55d97a90ab56ad554b04facfcdaa453019488b2

--- device.mk
+++ device.mk
@@ -27,7 +27,8 @@ AB_OTA_PARTITIONS += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_pahu.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_pahu.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml
+    $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml \
+    $(LOCAL_PATH)/audio/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
 # Camera
 PRODUCT_PACKAGES += \
