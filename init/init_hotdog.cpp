9bc76dd5d6f4a238d6ed153a5e2db34b2970390e
LuK1337 <priv.luk@gmail.com>
2021-08-24 19:06:46 +0200

    hotdog: init: remove fingerprint override
    Change-Id: I3a60867f3a9e5fa84b1aaef2eab2986683305213

--- init/init_hotdog.cpp
+++ init/init_hotdog.cpp
@@ -23,17 +23,6 @@ std::vector<std::string> ro_product_props_default_source_order = {
     "system.",
 };
 
-// copied from build/tools/releasetools/ota_from_target_files.py
-// but with "." at the end and empty entry
-std::vector<std::string> ro_fingerprints_default_source_order = {
-    "",
-    "product.",
-    "odm.",
-    "vendor.",
-    "system.",
-    "bootimage.",
-};
-
 void property_set(char const prop[], char const value[]) {
     prop_info *pi;
 
@@ -65,12 +54,6 @@ void vendor_load_properties()
         property_override(prop_name.c_str(), value.c_str(), false);
     };
 
-    const auto set_ro_fingerprint = [](const std::string &source,
-            const std::string &value) {
-        auto prop_name = "ro." + source + "build.fingerprint";
-        property_override(prop_name.c_str(), value.c_str(), false);
-    };
-
     int project_name = stoi(android::base::GetProperty("ro.boot.project_name", ""));
     int rf_version = stoi(android::base::GetProperty("ro.boot.rf_version", ""));
     if (project_name == 19801 ) {
@@ -87,9 +70,6 @@ void vendor_load_properties()
             for (const auto &source : ro_product_props_default_source_order) {
                 set_ro_product_prop(source, "model", "HD1913");
             }
-            for (const auto &source : ro_fingerprints_default_source_order) {
-                set_ro_fingerprint(source, "google/coral/coral:11/RQ3A.210805.001.A1/7474174:user/release-keys");
-            }
             for (const auto &source : ro_product_props_default_source_order) {
                 set_ro_product_prop(source, "name", "OnePlus7TPro_EEA");
             }
@@ -99,9 +79,6 @@ void vendor_load_properties()
             for (const auto &source : ro_product_props_default_source_order) {
                 set_ro_product_prop(source, "model", "HD1917");
             }
-            for (const auto &source : ro_fingerprints_default_source_order) {
-                set_ro_fingerprint(source, "google/coral/coral:11/RQ3A.210805.001.A1/7474174:user/release-keys");
-            }
             for (const auto &source : ro_product_props_default_source_order) {
                 set_ro_product_prop(source, "name", "OnePlus7TPro");
             }
@@ -110,9 +87,6 @@ void vendor_load_properties()
         /* Default to global */
             for (const auto &source : ro_product_props_default_source_order) {
                 set_ro_product_prop(source, "model", "HD1917");
-            }
-            for (const auto &source : ro_fingerprints_default_source_order) {
-                set_ro_fingerprint(source, "google/coral/coral:11/RQ3A.210805.001.A1/7474174:user/release-keys");
             }
             for (const auto &source : ro_product_props_default_source_order) {
                 set_ro_product_prop(source, "name", "OnePlus7TPro");
