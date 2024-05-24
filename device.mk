27706b65bcac10dab8455b09877c11f20bcba9f9
Julian Veit <Claymore1298@gmail.com>
2024-05-24 11:10:55 +0200

    [DNM]: hotdog: temp
    Change-Id: I5c28ea3b943b94a56c0fb3087c16df1742c18ac9

--- device.mk
+++ device.mk
@@ -53,3 +53,5 @@ $(call inherit-product, device/oneplus/sm8150-common/common.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/hotdog/hotdog-vendor.mk)
+
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
